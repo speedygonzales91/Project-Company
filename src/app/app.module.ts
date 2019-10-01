6f9a78326b51cebd4687271c40bc8fc96626902a
mozsolt <zsolt.molnar@fx.hu>
2019-10-01 17:20:50 +0200

    add more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    ClientsComponent,
+    PricingComponent,
+    FooterComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
